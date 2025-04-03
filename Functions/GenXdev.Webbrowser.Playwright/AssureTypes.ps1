5727a250bfae1c1cc0e455a83582e0363c42c930
René Vaessen <rene.vaessen@gmail.com>
2025-04-03 14:01:49 +0200

    Release 1.172.2025

--- Functions/GenXdev.Webbrowser.Playwright/AssureTypes.ps1
+++ Functions/GenXdev.Webbrowser.Playwright/AssureTypes.ps1
@@ -5,11 +5,11 @@ using namespace Microsoft.Playwright
 
 # load the main Playwright assembly from module's lib folder
 Microsoft.PowerShell.Utility\Add-Type -Path (Microsoft.PowerShell.Management\Join-Path $PSScriptRoot `
-        '..\..\..\..\GenXdev.Helpers\1.170.2025\lib\Microsoft.Playwright.dll')
+        '..\..\..\..\GenXdev.Helpers\1.172.2025\lib\Microsoft.Playwright.dll')
 
 # load the Playwright test adapter assembly
 Microsoft.PowerShell.Utility\Add-Type -Path (Microsoft.PowerShell.Management\Join-Path $PSScriptRoot `
-        '..\..\..\..\GenXdev.Helpers\1.170.2025\lib\Microsoft.Playwright.TestAdapter.dll')
+        '..\..\..\..\GenXdev.Helpers\1.172.2025\lib\Microsoft.Playwright.TestAdapter.dll')
 
 # initialize thread-safe dictionary to store browser instances
 $Global:GenXdevPlaywrightBrowserDictionary = `
