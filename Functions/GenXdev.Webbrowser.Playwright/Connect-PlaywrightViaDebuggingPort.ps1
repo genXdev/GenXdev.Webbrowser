67acd68ec18157a8c62d6aca71cd1e3ce7a4db65
René Vaessen <rene.vaessen@gmail.com>
2025-09-17 07:29:58 +0200

    Release 1.272.2025

--- Functions/GenXdev.Webbrowser.Playwright/Connect-PlaywrightViaDebuggingPort.ps1
+++ Functions/GenXdev.Webbrowser.Playwright/Connect-PlaywrightViaDebuggingPort.ps1
@@ -2,7 +2,7 @@
 Part of PowerShell module : GenXdev.Webbrowser.Playwright
 Original cmdlet filename  : Connect-PlaywrightViaDebuggingPort.ps1
 Original author           : René Vaessen / GenXdev
-Version                   : 1.270.2025
+Version                   : 1.272.2025
 ################################################################################
 MIT License
 
